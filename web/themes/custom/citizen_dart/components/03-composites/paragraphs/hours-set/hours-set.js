11587966941ac46ae5ad005f194a4344f08b2498
Adam Fuchs <adam@electriccitizen.com>
2023-09-29 13:55:47 -0500

    Add exceptions to hours paragraph and add display range functionality

--- web/themes/custom/citizen_dart/components/03-composites/paragraphs/hours-set/hours-set.js
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/hours-set/hours-set.js
@@ -3,14 +3,15 @@
   Drupal.behaviors.hours = {
     attach: function (context, settings) {
       $(once('hours', '.field-hours', context)).each(function(){
+      	$('.select-panel:first-of-type').addClass('show-panel');
         //change visibility of hours listings
         $('.panel-select', this).change(function(e){
           e.preventDefault();
           // visually hide all hours
-          $('.select-panel').addClass('visually-hidden');
+          $('.select-panel').removeClass('show-panel');
           let activeHours = $(this).children('option:selected').val();
           // show selected hours
-          $('.' + activeHours).removeClass('visually-hidden');
+          $('.' + activeHours).addClass('show-panel');
         });
       });
     }
