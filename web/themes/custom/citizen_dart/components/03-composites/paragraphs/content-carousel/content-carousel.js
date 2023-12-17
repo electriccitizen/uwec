1efe49eb2777c2ddd2d9705e843e270e48f55265
Dan Herrero <dan@dherrero.com>
2023-12-17 15:30:34 -0500

    UWEC-130: Content Carousel theming updates.

---
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/content-carousel/content-carousel.js
@@ -0,0 +1,20 @@
+
+(function($, Drupal, once) {
+
+	//Swipebox script for lightbox images.
+	Drupal.behaviors.contentCarousel = {
+		attach: function (context, settings) {
+			//Slider
+			$(once('contentCarousel', '.paragraph--type--content-carousel', context)).each(function(){
+				$(document).ready(function(){
+					$('.widget-wrapper', this).slick({
+						adaptiveHeight: false,
+						autoplay: false,
+						autoplaySpeed: 5000
+					});
+				});
+			});
+		}
+	}
+
+})(jQuery, Drupal, once);
