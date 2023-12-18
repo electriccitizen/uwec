a4e421476dd3b333f5bc0acf34211ca2cb1ac6fc
Matt Larson <larsomat@uwec.edu>
2023-12-18 16:18:14 -0600

    use new field_courses in courses block

--- web/modules/custom/citizen_custom/src/Plugin/Block/Courses.php
+++ web/modules/custom/citizen_custom/src/Plugin/Block/Courses.php
@@ -24,18 +24,16 @@ class Courses extends BlockBase{
 		}
 
 		// grab the courseleaf slug
-		$courseLeafSlug = $program->get('field_courseleaf_slug')->getValue();
-
-		// if this program doesn't have a CourseLeaf Slug, there is nothing we can do.
+		$courseLeafSlug = $this->getCourseLeafSlug($program);
 		if(empty($courseLeafSlug)){
 			return ['#markup'=>''];
 		}
 
-		// there can only ever be one, so just grab that to simplify
-		$courseLeafSlug = $courseLeafSlug[0]['value'];
+		// grab the manually entered course numbers from the program
+		$manualCourseNumbers = $this->getManualCourseNumbers($program);
 
 		// get courses from CourseLeaf
-		$courses = $this->getCourses($courseLeafSlug);
+		$courses = $this->getCourses($courseLeafSlug, $manualCourseNumbers);
 
 		return [
 			'#theme'=>'courses_block',
@@ -45,9 +43,32 @@ class Courses extends BlockBase{
 		];
 	}
 
+	// returns the course leaf slug for the given program.
+	// returns false if there isn't one.
+	protected function getCourseLeafSlug($program){
+		$courseLeafSlug = $program->get('field_courseleaf_slug')->getValue();
+		if(empty($courseLeafSlug)) return false;
+		$courseLeafSlug = $courseLeafSlug[0]['value'];
+		if(empty($courseLeafSlug)) return false;
+		return $courseLeafSlug;
+	}
+
+	// returns an array of manually entered course numbers.
+	// the array will be empty if there are none.
+	protected function getManualCourseNumbers($program){
+		$nums = $program->get('field_courses')->getValue();
+		if(empty($nums)) return [];
+
+		// convert array of arrays with key "value" to useful array
+		$nums = array_map(fn($arr) => $arr['value'], $nums);
+		return $nums;
+	}
+
 	// returns an array of courses
 	// each course is an array containing "title", "number", and "description"
-	protected function getCourses($slug){
+	// the course numbers provided in $manualCourseNumbers will be returned first, if they exist in the response.
+	// random courses will be selected if there are no more manual course numbers to choose from.
+	protected function getCourses($slug, $manualCourseNumbers){
 		$url = 'https://catalog.uwec.edu/ribbit/?page=getcourse.rjs&subject='.$slug; $response = \Drupal::httpClient()->request('GET', $url);
 
 		// anything but a 200 is bunk
@@ -55,14 +76,17 @@ class Courses extends BlockBase{
 			return [];
 		}
 
-		$courses = [];
 		$xml = new \SimpleXMLElement($response->getBody()->getContents());
 
 		// pick 3 random courses to parse
-		$random_indices = $this->getRandomIndices($xml->course->count());
+		//$random_indices = $this->getRandomIndices($xml->course->count());
 
-		foreach($random_indices as $course_idx){
-			$course = $xml->course[$course_idx];
+		//foreach($random_indices as $course_idx){
+
+		// gather all courses in an array, indexed by lowercase course number
+		$allCourses = [];
+		for($i = 0; $i < $xml->course->count(); $i++){
+			$course = $xml->course[$i];
 			$courseHtml = new \DOMDocument();
 			$courseHtml->loadHTML((string)$course);
 			$courseDiv = $courseHtml->documentElement->firstChild->firstChild;
@@ -80,10 +104,42 @@ class Courses extends BlockBase{
 					}
 				}
 			}
-			$courses[] = $courseData;
+			$allCourses[strtolower($courseData['number'])] = $courseData;
 		}
 
-		return $courses;
+		// ok now we try to pick the right 3 courses to show.
+		$returnedCourses = [];
+
+		// starting with the manually selected ones.
+		foreach($manualCourseNumbers as $courseNum){
+			$manualKey = strtolower($courseNum);
+			if(isset($allCourses[$manualKey])){
+				$returnedCourses[] = $allCourses[$manualKey];
+
+				// remove this course from the array of all courses
+				// so if we do a random selection later, this course cannot be chosen again.
+				unset($allCourses[$manualKey]);
+
+				// we only need 3
+				if(count($returnedCourses) >= 3) break;
+			}
+		}
+
+		// if we don't have 3 yet, pick some random ones:
+		if(count($returnedCourses) < 3){
+			$numCoursesToPick = 3 - count($returnedCourses);
+			$randomKeys = array_rand($allCourses, $numCoursesToPick);
+			if(is_array($randomKeys)){
+				foreach($randomKeys as $key){
+					$returnedCourses[] = $allCourses[$key];
+				}
+			}else{
+				// array_rand returns the key itself if $numCoursesToPick is 1.
+				$returnedCourses[] = $allCourses[$randomKeys];
+			}
+		}
+
+		return $returnedCourses;
 	}
 
 	// returns 3 random indices with the given $max
@@ -93,7 +149,7 @@ class Courses extends BlockBase{
 		if($max == 0) return [];
 
 		$indices = array_fill(0, $max, true);
-		
+
 		if($max > 3){
 			return array_rand($indices, 3);
 		}else{
