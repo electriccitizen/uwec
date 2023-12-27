eed94bc18fee4560f9c5cbda3666d0ac87ef7615
Dan Herrero <dan@dherrero.com>
2023-12-27 18:08:08 -0500

    Add in masonry functionality.

--- web/themes/custom/citizen_dart/components/03-composites/paragraphs/gallery/gallery.js
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/gallery/gallery.js
@@ -13,6 +13,7 @@
 						galleryFadeIn: 300,
 						openSpeed: 300
 					});
+					$('.')
 				});
 			});
 			//Slider
@@ -27,47 +28,12 @@
 			});
 			// Masonry
 			$(once('masonry', '.gallery-style.masonry', context)).each(function(){
-				let grids = [...document.querySelectorAll('.grid--masonry .gallery-wrapper')];
-
-				if(grids.length) {
-					grids = grids.map(grid => ({
-						_el: grid, 
-						gap: parseFloat(getComputedStyle(grid).gridRowGap), 
-						items: [...grid.childNodes].filter(c => c.nodeType === 1), 
-						ncol: 0
-					}));
-
-					function layout() {
-						grids.forEach(grid => {
-							/* get the post relayout number of columns */
-							let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
-
-							/* if the number of columns has changed */
-							if(grid.ncol !== ncol) {
-								/* update number of columns */
-								grid.ncol = ncol;
-
-								/* revert to initial positioning, no margin */
-								grid.items.forEach(c => c.style.removeProperty('margin-top'));
-
-								/* if we have more than one column */
-								if(grid.ncol > 1) {
-									grid.items.slice(ncol).forEach((c, i) => {
-										let prev_fin = grid.items[i].getBoundingClientRect().bottom /* bottom edge of item above */, 
-												curr_ini = c.getBoundingClientRect().top /* top edge of current item */;
-										
-										c.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`
-									})
-								}
-							}
-						})
-					}
-
-					addEventListener('load', e => {
-						layout(); /* initial load */
-						addEventListener('resize', layout, false) /* on resize */
-					}, false);
-				}
+				$(this).masonry({
+					// options
+					itemSelector: '.masonry-item',
+					columnWidth: 300,
+					gutter: 16
+				});
 			});
 		}
 	}
