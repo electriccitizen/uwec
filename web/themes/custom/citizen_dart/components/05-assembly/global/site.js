ae0e3be6d52ebf8db71157e66a6193b5b58d1424
Adam Fuchs <adam@electriccitizen.com>
2024-01-30 17:37:45 -0600

    Mobile footer theming

--- web/themes/custom/citizen_dart/components/05-assembly/global/site.js
+++ web/themes/custom/citizen_dart/components/05-assembly/global/site.js
@@ -22,14 +22,6 @@ Drupal.behaviors.removeEmptyRegions = {
 Drupal.behaviors.backToTop = {
   attach: function (context, settings) {
   	$(once('backTop', 'html.js', context)).each(function(){
-      $(window).scroll(function(){
-        var back = $(window).height() * .8;
-        if ($(this).scrollTop() > back ) {
-          $('.back-anchor').fadeIn(200);
-        }else{
-          $('.back-anchor').fadeOut(200);
-        }
-      });
       //scroll to toc
       $('.back-anchor a').click(function(e){
         e.preventDefault();
