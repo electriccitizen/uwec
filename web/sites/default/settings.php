898935acaa1dd951cf5eff6bea6862b94aa88146
david kirkwood <david@electriccitizen.com>
2024-02-01 09:00:05 -0600

    UWEC-3: Fix uid setting in migrations; adjustment to api_key setting.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -49,7 +49,7 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
       $config['config_split.config_split.dev']['status'] = TRUE;
       $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
       $secrets = json_decode(file_get_contents($secretsFile),  1);
-      $settings['uwec_api_key'] = $secrets['API_KEY'];
+      $settings['uwec_api_key'] = $secrets['uwec_api_key'];
       break;
 
   }
