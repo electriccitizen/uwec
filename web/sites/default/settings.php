1f21e33461ed7d8c01a33614c6f234fce0fb1c2e
david kirkwood <david@electriccitizen.com>
2024-01-31 21:22:28 -0600

    UWEC-3: The api key is stored in Drupal settings.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -49,7 +49,7 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
       $config['config_split.config_split.dev']['status'] = TRUE;
       $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
       $secrets = json_decode(file_get_contents($secretsFile,  1));
-      $_ENV['API_KEY'] = $secrets['API_KEY'];
+      $settings['uwec_api_key'] = $secrets['API_KEY'];
       break;
 
   }
