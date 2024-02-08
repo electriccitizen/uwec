d3be2d66225e5d0343ec6b3f65afb38275e7857d
david kirkwood <david@electriccitizen.com>
2024-02-08 17:06:00 -0600

    UWEC-3c: Added a progress bar to the unused entity delete script.

--- web/modules/custom/citizen_migrate/src/Commands/CmDeleteUnreferencedEntities.php
+++ web/modules/custom/citizen_migrate/src/Commands/CmDeleteUnreferencedEntities.php
@@ -5,6 +5,8 @@ namespace Drupal\citizen_migrate\Commands;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\FileSystemInterface;
 use Drush\Commands\DrushCommands;
+use Symfony\Component\Console\Helper\ProgressBar;
+
 
 /**
  * A Drush commandfile.
@@ -55,9 +57,6 @@ class CmDeleteUnreferencedEntities extends DrushCommands {
     $csv_path = 'public://source-data/active_image_ids.csv';
     $real_path = $this->fileSystem->realpath($csv_path);
 
-    /** @var \Drupal\citizen_migrate\Services\CmTools $cmTools */
-    $cmTools = \Drupal::service('cm.tools');
-
     if (!file_exists($real_path)) {
       $this->logger()->error(dt('CSV file does not exist at @path.', ['@path' => $real_path]));
       return;
@@ -71,14 +70,18 @@ class CmDeleteUnreferencedEntities extends DrushCommands {
     $csv_data = [];
     if (($handle = fopen($real_path, 'r')) !== FALSE) {
       while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
-        $csv_data[] = trim($data[4]);
+        $csv_data[] = trim($data[4]); // Adjust based on the correct column index in your CSV
       }
       fclose($handle);
     }
 
     $query = \Drupal::database()->select($table_name, 'm')
       ->fields('m', ['sourceid1', 'destid1']);
-    $results = $query->execute();
+    $results = $query->execute()->fetchAll();
+
+    // Initialize the progress bar
+    $progress = new ProgressBar($this->output(), count($results));
+    $progress->start();
 
     foreach ($results as $row) {
       if (!in_array($row->sourceid1, $csv_data)) {
@@ -86,9 +89,15 @@ class CmDeleteUnreferencedEntities extends DrushCommands {
         $entity = $this->entityTypeManager->getStorage($entity_type)->load($row->destid1);
         if ($entity) {
           $entity->delete();
-          $this->logger()->info('Deleted @type entity with ID @id.', ['@type' => $entity_type, '@id' => $row->destid1]);
+          $progress->advance();
         }
+      } else {
+        $progress->advance();
       }
     }
+
+    // Finish the progress bar when the operation is complete
+    $progress->finish();
+    $this->logger()->info('Completed deleting unreferenced entities.');
   }
 }
