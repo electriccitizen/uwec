6f3abd6bb896215ac22b7c07badffa09be144254
Adam Fuchs <adam@electriccitizen.com>
2024-02-14 17:50:49 -0600

    Slate form theming

---
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/slate-form/slate-form.js
@@ -0,0 +1,42 @@
+(function($, Drupal, once) {
+
+/* SLATE ANIMATIONS
+------------------ */
+Drupal.behaviors.slateAnimate = {
+  attach: function (context, settings) {
+  	$(once('slateAnimations', '.slate-left', context)).each(function(){
+      // Function to handle the intersection observer callback
+      function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              //add class when target is visible
+              entry.target.classList.add('slate-visible');
+              observer.unobserve(entry.target); // Stop observing once the class is added
+            }
+        });
+      }
+      // Create an intersection observer
+      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+      // Select the target element
+      // Start observing the target element
+      observer.observe(this);
+    });
+    $(once('slateAnimations', '.slate-right', context)).each(function(){
+      //find option selected so can change the color of slate select
+      $(document).ready(function(){
+        setTimeout(function() {
+          $('.slate-right select').change(function () {
+            const selected = $(this).find('option:selected').val();
+            if(selected != ''){
+              $(this).addClass('selected');
+            }else{
+              $(this).removeClass('selected');
+            }
+          });
+        }, 500);
+      });
+    });
+  }
+}
+
+})(jQuery, Drupal, once);
