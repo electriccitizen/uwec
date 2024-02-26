3bb7f09606acc0beb3b779888ca03cc5930edd23
Adam Fuchs <adam@electriccitizen.com>
2024-02-26 13:25:01 -0600

    Add select type animation, do not require select item text

---
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/select-type/select-type.js
@@ -0,0 +1,27 @@
+(function($, Drupal, once) {
+
+/* SELECT TYPE ANIMATIONS
+------------------ */
+Drupal.behaviors.selectTypeAnimate = {
+  attach: function (context, settings) {
+  	$(once('selectTypeAnimations', '.paragraph--type--select-type', context)).each(function(){
+      // Function to handle the intersection observer callback
+      function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            //add class when target is visible
+            entry.target.classList.add('select-visible');
+            observer.unobserve(entry.target); // Stop observing once the class is added
+          }
+        });
+      }
+      // Create an intersection observer
+      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.25 });
+      // Select the target element
+      // Start observing the target element
+      observer.observe(this);
+    });
+  }
+}
+
+})(jQuery, Drupal, once);
