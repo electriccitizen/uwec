26ad10e2619b5b8162c8edc9e95e69d4526bb540
Adam Fuchs <adam@electriccitizen.com>
2024-03-11 13:34:17 -0500

    Wayfinding theming and animation

---
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/wayfinding/wayfinding.js
@@ -0,0 +1,27 @@
+(function($, Drupal, once) {
+
+/* WAYFINDING ANIMATIONS
+------------------ */
+Drupal.behaviors.wayfindingAnimate = {
+  attach: function (context, settings) {
+  	$(once('wayfindingAnimations', '.paragraph--type--wayfinding .wayfinding', context)).each(function(){
+      // Function to handle the intersection observer callback
+      function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            //add class when target is visible
+            entry.target.classList.add('wayfinding-visible');
+            observer.unobserve(entry.target); // Stop observing once the class is added
+          }
+        });
+      }
+      // Create an intersection observer
+      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.15 });
+      // Select the target element
+      // Start observing the target element
+      observer.observe(this);
+    });
+  }
+}
+
+})(jQuery, Drupal, once);
