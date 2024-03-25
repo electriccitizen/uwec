39c5c616845a9d0717a998cbbd272244a13547b2
Adam Fuchs <adam@electriccitizen.com>
2024-03-25 09:02:54 -0500

    Section nav fixed position, desktop theming

--- web/themes/custom/citizen_dart/components/02-chunks/navigation/section-menu/section-menu.js
+++ web/themes/custom/citizen_dart/components/02-chunks/navigation/section-menu/section-menu.js
@@ -7,9 +7,9 @@ Drupal.behaviors.sectionMenu = {
 			$('.section-menu-toggle').click(function(e){
 				e.preventDefault();
         if($(this).is('.active-nav')){
-          $(this).attr('aria-expanded', 'false').removeClass('active-nav').find('span').text('Explore Section').closest('#section-menu-title').next('#section-menu-wrapper').attr('aria-hidden', 'true').slideUp(500);
+          $(this).attr('aria-expanded', 'false').removeClass('active-nav').find('span').text('Explore Section').removeClass('expanded').closest('#section-menu-title').next('#section-menu-wrapper').attr('aria-hidden', 'true').slideUp(500);
         }else{
-          $(this).attr('aria-expanded', 'true').addClass('active-nav').find('span').text('Close').addClass('active-nav').closest('#section-menu-title').next('#section-menu-wrapper').attr('aria-hidden', 'false').slideDown(500);
+          $(this).attr('aria-expanded', 'true').addClass('active-nav').find('span').text('Close').addClass('expanded').closest('#section-menu-title').next('#section-menu-wrapper').attr('aria-hidden', 'false').slideDown(500);
         }
 			});
 
@@ -20,12 +20,12 @@ Drupal.behaviors.sectionMenu = {
 				$('#node-section-2 > .layout--twocol-sideleft > .layout__region--first').remove();
 			}
 		});
-	}
+	}//end context attach
 }//end section menu function
 
 function mobileSectionnav() {
   var wwidth = $(window).outerWidth();
-  if (wwidth < 984) {
+  if (wwidth < 1200) {
   	$('.section-menu-toggle').attr('href','#');
     //add aria roles to menu title and wrapper if not already set by click above
     if(!$('.section-menu-toggle').attr('aria-controls')){
@@ -42,5 +42,40 @@ function mobileSectionnav() {
   }
 };
 
+/* DETECT POSITION
+------------------ */
+Drupal.behaviors.navPosition = {
+  attach: function (context, settings) {
+  	$(once('navPosition', '.layout--twocol-sideleft', context)).each(function(){
+      $(document).ready(function() {
+        var windowWidth = $(window).outerWidth();
+        var $targetElement = $('.layout--twocol-sideleft'); 
+        var $titleHeight = $('#section-menu-title').outerHeight();
+        var viewportBottom = $(window).scrollTop() + $(window).height();
+        var elementTop = $targetElement.offset().top;
+        if(windowWidth > 1199){
+          $('#block-section-menu',$targetElement).css('top','calc(100% - ' + $titleHeight + 'px)');
+          console.log(viewportBottom,elementTop);
+          if (viewportBottom > elementTop) {
+            $('#block-section-menu',$targetElement).addClass('post-intro');
+          } else {
+            $('#block-section-menu',$targetElement).removeClass('post-intro');
+          }
+        }else{
+          $('#block-section-menu',$targetElement).css('top','auto');
+        }
+        $(window).scroll(function() {
+          var viewportBottom = $(window).scrollTop() + $(window).height();
+          var elementTop = $targetElement.offset().top;
+          if (viewportBottom > elementTop) {
+            $('#block-section-menu',$targetElement).addClass('post-intro');
+          } else {
+            $('#block-section-menu',$targetElement).removeClass('post-intro');
+          }
+        });
+      });
+    });
+  }
+}
 
 })(jQuery, Drupal, once);
