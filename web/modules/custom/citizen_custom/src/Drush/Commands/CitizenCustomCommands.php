8a432031ea2a7699798e6d43fcb856728a088a97
Matt Larson <larsomat@uwec.edu>
2025-01-31 09:50:41 -0600

    add drush cmd citizen_custom:delete_revisions
    it deletes all non-current revisions for a given content type

--- web/modules/custom/citizen_custom/src/Drush/Commands/CitizenCustomCommands.php
+++ web/modules/custom/citizen_custom/src/Drush/Commands/CitizenCustomCommands.php
@@ -94,4 +94,53 @@ final class CitizenCustomCommands extends DrushCommands {
 
 		$this->logger()->success('Set defaults on '.$count.' profiles.');
 	}
+
+	#[CLI\Command(name: 'citizen_custom:delete_revisions')]
+	public function delete_revisions($content_type){
+		// validate content_type
+		$content_types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
+		$content_types = array_keys($content_types);
+		if(!in_array($content_type, $content_types)){
+			$this->output->writeln('Invalid content type "'.$content_type.'". Try one of these: '.implode(', ', $content_types));
+			return;
+		}
+
+		$nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+
+		// first load all nodes for the given content type
+		$nodes = $nodeStorage->loadByProperties([
+			'type'=>$content_type,
+		]);
+
+		$this->io()->info('Found '.count($nodes).' "'.$content_type.'" nodes. I will now delete all non-current revisions on those.');
+
+		$this->io()->progressStart(count($nodes));
+
+		// load all revisions,
+		// and delete all of them, unless they're the default revision.
+		$total_delete_count = 0;
+		foreach($nodes as $node){
+			$node_delete_count = 0;
+
+			$vids = $nodeStorage->revisionIds($node);
+			foreach($vids as $vid){
+				if($node->getLoadedRevisionId() != $vid){
+					try{
+						$nodeStorage->deleteRevision($vid);
+						$node_delete_count++;
+						$total_delete_count++;
+					}catch(\Exception $e){
+						// tried to delete the default revision
+						$this->io()->warning('I was about to delete the current revision of node '.$node->id().' but I did not.');
+					}
+				}
+			}
+
+			$this->io()->progressAdvance();
+		}
+
+		$this->io()->progressFinish();
+
+		$this->io()->success('Done! Deleted '.$total_delete_count.' revisions in total.');
+	}
 }
