46ccc8def389f1bc87d890d3d450489092fad968
Matt Larson <larsomat@uwec.edu>
2025-06-24 14:51:47 -0500

    custom error page

---
+++ web/modules/custom/citizen_custom/error.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="cache-control" content="no-cache, no-store, max-age=0" />
+	<meta http-equiv="expires" content="-1" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<title>University of Wisconsin-Eau Claire</title>
+<style>
+body {
+	font:100%/1.5 'Trebuchet MS',Arial,Helvetica,sans-serif;
+	background:#ecede9;
+	color:#555f60;
+	margin:0;
+	-webkit-font-smoothing:antialiased;
+	-moz-osx-font-smoothing:grayscale;
+}
+a:link,
+a:visited {
+	text-decoration:none;
+	color:#2b3e85;
+}
+a:hover,
+a:active {
+	color:#888178;
+}
+img {
+	max-width:100%;
+	display:block;
+}
+
+/* Headings */
+h1, h2, h3, h4, h5, h6 {
+	font-family:'Trebuchet MS',Arial,Helvetica,sans-serif;
+	font-weight:normal;
+	color:#222;
+}
+h2 {
+	font-size:1.65rem;
+	margin:1rem 0 0;
+}
+h3 {
+	font-size:1.25rem;
+	margin:1rem 0 0.5rem;
+}
+h2+h3 {
+	margin-top:0;
+}
+h5 {
+	font-size:1.1rem;
+}
+
+/* Header */
+header {
+	position:relative;
+	padding:0.5rem 1.75rem;
+	margin:0 auto;
+}
+header h1 {
+	display:block;
+	text-indent:-9999em;
+	max-width:30rem;
+	height:auto;
+	margin:0 0 0 -1rem;
+	background:transparent url('https://cdn.uwec.edu/uwec16/img/sprites/wordmark-horizontal.svg') no-repeat 25% center;
+	background-size:500% auto;
+	font-family:'Times New Roman',Times,serif;
+	line-height:35px; /* Height of logo */
+	color:#fff;
+}
+#top-links {
+	position:absolute;
+	text-align:right;
+	right:1.5rem;
+	width:57%;
+	top:1rem;
+}
+#top-links ul {
+	list-style:none;
+	padding:0;
+	margin:0;
+}
+#top-links li {
+	display:inline;
+	margin:0 4% 0 0;
+}
+#top-links li a {
+	color:#fff;
+}
+
+/* Text elements */
+p {
+	margin:0.6rem 0;
+}
+ul {
+	margin:0.5rem 0 1rem;
+	padding:0 0 0 1rem;
+}
+
+/* Layout */
+main {
+	display:block;
+	background:#fff;
+	padding:1.5rem 0.9375rem 0.9375rem;
+}
+.panel {
+	padding:0.75rem;
+	background:#f2f2f2;
+	border:1px solid #d9d9d9;
+}
+
+/* Side panel */
+#sidebar .columns {
+	padding:0;
+	margin:0;
+}
+#sidebar .panel {
+	padding:0 1.25rem;
+	border:none;
+}
+#sidebar .panel h5 {
+	line-height:1.2;
+	padding:0.5rem 1.25rem 0.25rem;
+	margin:0 -1.25rem 0.6rem;
+	background:#555F60;
+	color:#fff;
+}
+.side-nav {
+	list-style:none;
+	padding:0 0 1rem;
+}
+.side-nav a {
+	color:#fff !important;
+	font-weight:bold;
+}
+
+/* Footer */
+footer {
+	text-align:center;
+	padding:0 0.9375rem;
+}
+footer p {
+	/*border-top:1px solid #ddd;*/
+	padding:0.75rem 0;
+}
+#tech-info {
+	display:block;
+	margin:1rem 0;
+	line-height:1.25;
+}
+
+/* Brand colors */
+.brand-blue-light-bk { background-color:#7badb6; }
+.brand-orange-bk { background-color:#e37a33; }
+.brand-blue-bk { background-color:#2b3e85; }
+.brand-red-bk { background-color:#b4373c; }
+
+/* Medium and small screens */
+@media only screen and (max-width:64em) {
+	/* Show/hide */
+	.show-large {
+		display:none;
+	}
+	/* Don't float that nav bar */
+	#top-links {
+		width:auto;
+		position:static;
+		text-align:left;
+	}
+}
+
+/* Medium screens */
+@media only screen and (min-width:40em) and (max-width:64em) {
+	/* Float the columns */
+	#sidebar {
+		display:flex;
+	}
+	#sidebar>div {
+		flex:1;
+	}
+
+	/* Add a gutter between the panels */
+	#sidebar>div:first-child .panel {
+		border-right:0.75rem solid white;
+	}
+	#sidebar>div:last-child .panel {
+		border-left:0.75rem solid white;
+	}
+}
+
+/* Large screens */
+@media screen and (min-width:64.0625rem) {
+	header {
+		max-width:64rem;
+		box-shadow:-128rem 0 0 #2b3e85, -64rem 0 0 #2b3e85, 64rem 0 0 #2b3e85, 128rem 0 0 #2b3e85;
+	}
+	header h1 {
+		max-width:30rem;
+		margin-left:0;
+	}
+	main {
+		display:flex;
+		justify-content:center;
+	}
+	#content {
+		box-sizing:border-box;
+		padding:0 1rem;
+		max-width:48rem;
+		flex:0 1 48rem;
+		order:2;
+	}
+	#sidebar {
+		box-sizing:border-box;
+		padding:0 1rem;
+		max-width:16rem;
+		flex:0 1 16rem;
+		order:1;
+	}
+	/* Show/hide */
+	.hide-large {
+		display:none;
+	}
+}
+</style>
+</head>
+<body>
+<!-- Nav Bar -->
+<header class="brand-blue-bk">
+	<h1>University of Wisconsin-Eau Claire</h1>
+	<nav id="top-links">
+		<ul>
+			<li><a href="https://apply.wisconsin.edu/" target="_blank"><strong>Apply</strong></a></li>
+			<li><a href="https://webmail.uwec.edu/" target="_blank">Webmail</a></li>
+			<li><a href="https://uweau.instructure.com/login/saml/8" target="_blank">Canvas Login</a></li>
+			<li><a href="https://camps.uwec.edu/" target="_blank">CampS</a></li>
+			<li><a href="http://directory.uwec.edu/" target="_blank">Directory</a></li>
+		</ul>
+	</nav>
+</header>
+<!-- End Nav -->
+<main>
+	<article id="content" role="content">
+		<h2>The UW-Eau Claire website is currently unavailable</h2>
+		<h3>Reason: Internal Server Error</h3>
+		<p>You can still connect with UW-Eau Claire!</p>
+		<p>Please see below for office contact information, as well as links to critical services. For other questions, contact the Help Desk at <a href="tel:+17158365711">715-836-5711</a> or <a href="mailto:helpdesk@uwec.edu">helpdesk@uwec.edu</a>.</p>
+
+		<h3>Contact</h3>
+		<ul>
+			<li>ASK Center: <a href="tel:+17158363131">715-836-3131</a>, <a href="mailto:askcenter@uwec.edu">askcenter@uwec.edu</a></li>
+			<li>Admissions Office: <a href="tel:+17158365415">715-836-5415</a>, <a href="mailto:admissions@uwec.edu">admissions@uwec.edu</a></li>
+			<li>Advising, Retention and Career Center (ARCC): <a href="tel:+17158363487">715-836-3487</a>, <a href="mailto:arcc@uwec.edu">arcc@uwec.edu</a></li>
+			<li>Barron Campus: <a href="tel:+17157886244">715-788-6244</a>, <a href="mailto:uwecbcinfo@uwec.edu">uwecbcinfo@uwec.edu</a></li>
+			<li>Blugold Central: <a href="tel:+17158363000">715-836-3000</a>, <a href="mailto:blugoldcentral@uwec.edu">blugoldcentral@uwec.edu</a></li>
+			<li>Bursar/Cashier's Office: <a href="tel:+17158365907">715-836-5907</a>, <a href="mailto:cashier@uwec.edu">cashier@uwec.edu</a></li>
+			<li>Campus Police: <a href="tel:+17158362222">715-836-2222</a> (911 for emergencies), <a href="mailto:police@uwec.edu">police@uwec.edu</a></li>
+			<li>Continuing Education (program registration): <a href="tel:+18668932423">1-866-893-2423</a>, <a href="mailto:ce@uwec.edu">ce@uwec.edu</a></li>
+			<li>Counseling Services: <a href="tel:+17158365521">715-836-5521</a>, <a href="mailto:counsl01@uwec.edu">counsl01@uwec.edu</a></li>
+			<li>Financial Aid Office: <a href="tel:+17158363373">715-836-3373</a>, <a href="mailto:finaid@uwec.edu">finaid@uwec.edu</a></li>
+			<li>Human Resources: <a href="tel:+17158362513">715-836-2513</a>, <a href="mailto:humanresources@uwec.edu">humanresources@uwec.edu</a></li>
+			<li>Records and Registration: <a href="tel:+17158365912">715-836-5912</a>, <a href="mailto:registrar@uwec.edu">registrar@uwec.edu</a></li>
+			<li>Service Center (info and event tickets): <a href="tel:+17158364636">715-836-4636</a>, <a href="mailto:campusinfo@uwec.edu">campusinfo@uwec.edu</a></li>
+			<li>Student Health Service: <a href="tel:+17158364311">715-836-4311</a>, <a href="mailto:shs@uwec.edu">shs@uwec.edu</a></li>
+			<li>University Centers: <a href="tel:+17158364636">715-836-4636</a>, <a href="mailto:centers@uwec.edu">centers@uwec.edu</a></li>
+			<li>Visitor Parking: <a href="tel:+17158362544">715-836-2544</a>, <a href="mailto:blugoldcentral@uwec.edu">blugoldcentral@uwec.edu</a></li>
+		</ul>
+
+		<h3>Resources</h3>
+		<ul>
+			<li><a href="https://apply.wisconsin.edu/">Apply Online</a></li>
+			<li><a href="https://universityofwieauclaire.sharepoint.com/">UWEC Intranet</a></li>
+			<li><a href="https://directory.uwec.edu/">Campus Directory</a></li>
+			<li><a href="https://uweau.instructure.com/login/saml/8">Canvas</a></li>
+			<li><a href="https://eform1.uwec.edu/">Eforms (BP Logix)</a></li>
+			<li><a href="https://calendar.uwec.edu/">Events Calendar</a></li>
+			<li><a href="https://rms.uwec.edu/">Housing Portal</a></li>
+			<li><a href="https://library.uwec.edu">McIntyre Library</a></li>
+			<li><a href="https://my.uwec.edu/psp/CS90PRD/EMPLOYEE/HRMS/h/?tab=DEFAULT">MyBlugold CampS</a></li>
+			<li><a href="https://www.hrs.wisconsin.edu/">My UW System</a></li>
+			<li><a href="https://universityofwieauclaire.sharepoint.com/sites/registrar/Pages/default.aspx">Records and Registration Resources</a></li>
+			<li><a href="https://www.parchment.com/u/registration/34855/institution">Transcript Request</a></li>
+			<li><a href="https://webmail.uwec.edu/">Webmail</a></li>
+			<li><a href="https://wiser.wisc.edu/">WISER</a></li>
+		</ul>
+	</article>
+	<aside id="sidebar" style="order:3"></aside>
+</main>
+<!-- Footer -->
+<footer>
+	<p>In the meantime, check in with us on
+		<a href="https://www.facebook.com/uweauclaire/">Facebook</a>,
+		<a href="https://www.instagram.com/uweauclaire/">Instagram</a>, and
+		<a href="https://x.com/UWEauClaire">X</a>
+		Copyright &copy; <?=date('Y')?> <a href="https://www.uwec.edu/">UW-Eau Claire</a> and the Board of Regents of the <a href="http://www.wisconsin.edu/" target="_blank">Universities of Wisconsin</a></p>
+	<small id="tech-info"><strong>Technical Information</strong>
+		<br><?=$exception->getMessage()?>
+		<br>on line <?=$exception->getLine()?> in <?=$exception->getFile()?>
+	</small>
+</footer>
+</body>
+</html>
