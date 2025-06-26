22964a07d60efb62242ec397511ade5b93dc6cd4
Matt Larson <larsomat@uwec.edu>
2025-06-26 11:01:50 -0500

    header search: any unfocus closes search header

--- web/modules/custom/citizen_google_search/js/citizen_google_search.js
+++ web/modules/custom/citizen_google_search/js/citizen_google_search.js
@@ -1,26 +1,26 @@
 (function($, Drupal) {
-  Drupal.behaviors.googleCustomSearch = {
-    attach: function (context, settings) {
-      $(once('hasSearch', '.google-search', context)).each(function(){
-        document.onreadystatechange = function () {
-          if (document.readyState === 'complete') {
-            let form = document.querySelector('form.gsc-search-box');
-            let input = document.querySelector('input.gsc-input');
+	Drupal.behaviors.googleCustomSearch = {
+		attach: function (context, settings) {
+			$(once('hasSearch', '.google-search', context)).each(function(){
+				document.onreadystatechange = function () {
+					if (document.readyState === 'complete') {
+						let form = document.querySelector('form.gsc-search-box');
+						let input = document.querySelector('input.gsc-input');
 
-            let label = document.createElement("label");
-            label.htmlFor = 'gsc-i-id1';
-            label.innerHTML = 'Search the UW-Eau Claire Website';
-            
-            form.prepend(label);
-            input.setAttribute('placeholder', 'What are you searching for?');
-            input.style.backgroundImage = 'none';
-            document.querySelector('button.gsc-search-button svg').remove();
-            document.querySelector('button.gsc-search-button').append('Search');
-          }
-        };
-      });
-    }
-  };
+						let label = document.createElement("label");
+						label.htmlFor = 'gsc-i-id1';
+						label.innerHTML = 'Search the UW-Eau Claire Website';
+						
+						form.prepend(label);
+						input.setAttribute('placeholder', 'What are you searching for?');
+						input.style.backgroundImage = 'none';
+						document.querySelector('button.gsc-search-button svg').remove();
+						document.querySelector('button.gsc-search-button').append('Search');
+					}
+				};
+			});
+		}
+	};
 
 	function setHeaderSearchVisible(visible){
 		let $container = $('.google-search-container');
@@ -47,12 +47,8 @@
 	//search toggle
 	Drupal.behaviors.searchToggle = {
 		attach: function (context, settings) {
-		 	$(once('tSearch', '.block-citizen-google-search-block', context)).each(function(){
+			$(once('tSearch', '.block-citizen-google-search-block', context)).each(function(){
 				$('.search-form-container').on('focusout', function(e){
-					// ignore this event if nothing is receiving focus
-					// in other words, if somebody clicks in empty space, do nothing
-					if(e.relatedTarget == null) return;
-
 					// if focus goes outside the container, hide the container
 					// this is so the newly focused element is not obscured after tabbing out of the container.
 					if(!this.contains(e.relatedTarget)){
@@ -69,50 +65,48 @@
 					// ignore this if we are clicking the open/close button
 					if(e.target.classList.contains('t-search')) return;
 
-					// if this click it outside the search container, close
+					// if this click is outside the search container, close
 					if(!searchContainer.contains(e.target)){
 						setHeaderSearchVisible(false);
 					}
 				});
 
-
-		 		// find the top position of the site header and position the fixed search box to that if we're on desktop 
-		 		if($('window').outerWidth() > 1199){
-		 			var headerPosition = $('header.site-header').offset().top();
-		 			$('#header-search').css('top',headerPosition);
-		 		}
+				// find the top position of the site header and position the fixed search box to that if we're on desktop 
+				if($('window').outerWidth() > 1199){
+					var headerPosition = $('header.site-header').offset().top();
+					$('#header-search').css('top',headerPosition);
+				}
 
 				// toggle nav on click on search icon (or close search icon)
-		 		$('.t-search', this).click(function(e){
-		 			e.preventDefault();
+				$('.t-search', this).click(function(e){
+					e.preventDefault();
 					if($(this).hasClass('close-search')) {
 						setHeaderSearchVisible(false);
 					} else {
 						setHeaderSearchVisible(true);
 					}
-		 		});
+				});
 			});
 		}
 	}
 
-	/* Scroll to the results when a use searches
-	------------------------------------ */
+	// Scroll to the results when a user searches
 	Drupal.behaviors.searchResults = {
-	  attach: function (context, settings) {
-	    $(once('isSearching', '#search-results-wrapper', context)).each(function(){
-	      //check if searched and scroll to if so
-	      var urlCurrent = window.location.href;
-	      if((urlCurrent.indexOf("?q") > -1) || (urlCurrent.indexOf("#gsc.tab=0&gsc.q=") > -1)){
+		attach: function (context, settings) {
+			$(once('isSearching', '#search-results-wrapper', context)).each(function(){
+				//check if searched and scroll to if so
+				var urlCurrent = window.location.href;
+				if((urlCurrent.indexOf("?q") > -1) || (urlCurrent.indexOf("#gsc.tab=0&gsc.q=") > -1)){
 					$(document).ready(function(){
 						setTimeout(function() {
-			   			$('html,body').animate({
-							scrollTop: $('#search-results-wrapper').offset().top - 100
-			   			});
-		   			}, 300);
-			 		});
-	      }
-	    });
-	  }
+							$('html,body').animate({
+								scrollTop: $('#search-results-wrapper').offset().top - 100
+							});
+						}, 300);
+					});
+				}
+			});
+		}
 	};
 
 })(jQuery, Drupal);
