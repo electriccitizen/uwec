23816288a71d81592f5168053e5026ac542180a5
Matt Larson <larsomat@uwec.edu>
2025-10-02 13:29:05 -0500

    maybe done

--- web/modules/custom/citizen_google_search/js/citizen_google_search.js
+++ web/modules/custom/citizen_google_search/js/citizen_google_search.js
@@ -1,63 +1,56 @@
-(function($, Drupal) {
-	Drupal.behaviors.googleCustomSearch = {
-		attach: function (context, settings) {
-			$(once('hasSearch', '.google-search', context)).each(function(){
-				document.onreadystatechange = function () {
-					if (document.readyState === 'complete') {
-						let form = document.querySelector('form.gsc-search-box');
-						let input = document.querySelector('input.gsc-input');
+// tweak the google search form's html in the header
+// this uses the window "load" event so it fires after the google search form js
+// because the google js creates these elements
+window.addEventListener('load', ()=>{
+	let form = document.querySelector('form.gsc-search-box');
+	let input = document.querySelector('input.gsc-input');
 
-						let label = document.createElement("label");
-						label.htmlFor = 'gsc-i-id1';
-						label.innerHTML = 'Search the UW-Eau Claire Website';
-						
-						form.prepend(label);
-						input.setAttribute('placeholder', 'What are you searching for?');
-						input.style.backgroundImage = 'none';
-						document.querySelector('button.gsc-search-button svg').remove();
-						document.querySelector('button.gsc-search-button').append('Search');
-					}
-				};
-			});
-		}
-	};
+	let label = document.createElement("label");
+	label.htmlFor = 'gsc-i-id1';
+	label.innerHTML = 'Search the UW-Eau Claire Website';
+	form.prepend(label);
 
+	input.setAttribute('placeholder', 'What are you searching for?');
+	document.querySelector('button.gsc-search-button').append('Search');
+});
+
+(function(Drupal) {
 	function setHeaderSearchVisible(visible){
-		let $container = $('.google-search-container');
-		let $tSearch = $('.t-search');
+		let container = document.querySelector('.google-search-container');
+		let tSearch = document.querySelector('.t-search');
 
 		if(visible){
-			$container.addClass('expanded');
-			$container.attr('aria-hidden', 'false');
-			$tSearch.addClass('close-search');
-			$tSearch.find('span').text('Close search');
+			container.classList.add('expanded');
+			container.setAttribute('aria-hidden', 'false');
+			tSearch.classList.add('close-search');
+			tSearch.querySelector('span').innerHTML = 'Close search';
 
 			// auto focus on the search input
 			setTimeout(function(){
-				$('.gsc-input').focus();
+				document.querySelector('input.gsc-input').focus();
 			}, 100);
 		}else{
-			$container.removeClass('expanded');
-			$container.attr('aria-hidden', 'true');
-			$tSearch.removeClass('close-search');
-			$tSearch.find('span').text('Search');
+			container.classList.remove('expanded');
+			container.setAttribute('aria-hidden', 'true');
+			tSearch.classList.remove('close-search');
+			tSearch.querySelector('span').innerHTML = 'Search';
 		}
 	}
 
-	//search toggle
+	// desktop search toggle
 	Drupal.behaviors.searchToggle = {
 		attach: function (context, settings) {
-			$(once('tSearch', '.block-citizen-google-search-block', context)).each(function(){
-				$('.search-form-container').on('focusout', function(e){
-					// if focus goes outside the container, hide the container
-					// this is so the newly focused element is not obscured after tabbing out of the container.
-					if(!this.contains(e.relatedTarget)){
-						setHeaderSearchVisible(false);
-					}
+			once('tSearch', '.block-citizen-google-search-block', context).forEach(function(block){
+				block.querySelectorAll('.search-form-container').forEach((ele)=>{
+					ele.addEventListener('focusout', (event)=>{
+						if(!ele.contains(event.relatedTarget)){
+							setHeaderSearchVisible(false);
+						}
+					});
 				});
 
 				// close the header search if they click out
-				let searchContainer = document.querySelector('.google-search-container');
+				let searchContainer = block.querySelector('.google-search-container');
 				document.addEventListener('click', function(e){
 					// ignore this if it's already closed
 					if(!searchContainer.classList.contains('expanded')) return;
@@ -71,20 +64,16 @@
 					}
 				});
 
-				// find the top position of the site header and position the fixed search box to that if we're on desktop 
-				if($('window').outerWidth() > 1199){
-					var headerPosition = $('header.site-header').offset().top();
-					$('#header-search').css('top',headerPosition);
-				}
-
 				// toggle nav on click on search icon (or close search icon)
-				$('.t-search', this).click(function(e){
-					e.preventDefault();
-					if($(this).hasClass('close-search')) {
-						setHeaderSearchVisible(false);
-					} else {
-						setHeaderSearchVisible(true);
-					}
+				document.querySelectorAll('.t-search').forEach((tSearch)=>{
+					tSearch.addEventListener('click', (e)=>{
+						e.preventDefault();
+						if(tSearch.classList.contains('close-search')){
+							setHeaderSearchVisible(false);
+						} else {
+							setHeaderSearchVisible(true);
+						}
+					});
 				});
 			});
 		}
@@ -93,20 +82,20 @@
 	// Scroll to the results when a user searches
 	Drupal.behaviors.searchResults = {
 		attach: function (context, settings) {
-			$(once('isSearching', '#search-results-wrapper', context)).each(function(){
+			once('isSearching', '#search-results-wrapper', context).forEach(function(searchResultsWrapper){
 				//check if searched and scroll to if so
-				var urlCurrent = window.location.href;
-				if((urlCurrent.indexOf("?q") > -1) || (urlCurrent.indexOf("#gsc.tab=0&gsc.q=") > -1)){
-					$(document).ready(function(){
-						setTimeout(function() {
-							$('html,body').animate({
-								scrollTop: $('#search-results-wrapper').offset().top - 100
-							});
-						}, 300);
+				let urlCurrent = window.location.href;
+				if(urlCurrent.indexOf("?q") > -1){
+					let scrollTarget = searchResultsWrapper.getBoundingClientRect().top;
+					scrollTarget += window.scrollY; // ignore initial scroll
+					scrollTarget -= 100; // a bit of margin
+					window.scrollTo({
+						top: scrollTarget,
+						behavior: 'smooth',
 					});
 				}
 			});
 		}
 	};
 
-})(jQuery, Drupal);
+})(Drupal);
