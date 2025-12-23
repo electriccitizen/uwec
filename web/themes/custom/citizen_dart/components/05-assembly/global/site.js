f37af76f7bb043f4517fbbaaef39a901825a3c3f
Adam Fuchs <adam@electriccitizen.com>
2025-12-23 11:40:18 -0600

    Remove old footer animation JS

--- web/themes/custom/citizen_dart/components/05-assembly/global/site.js
+++ web/themes/custom/citizen_dart/components/05-assembly/global/site.js
@@ -20,35 +20,6 @@ Drupal.behaviors.removeEmptyRegions = {
   }
 }
 
-/* FOOTER ANIMATIONS
------------------- */
-Drupal.behaviors.footerAnimate = {
-  attach: function (context, settings) {
-  	$(once('footerAnimations', '.prefooter-wrapper', context)).each(function(){
-      // Function to handle the intersection observer callback
-      function handleIntersection(entries, observer) {
-        entries.forEach(entry => {
-          if ($("html").hasClass("animations-paused")) {
-            entry.target.classList.add('visible');
-            observer.unobserve(entry.target);
-          }
-          else if (entry.isIntersecting) {
-            //add class when target is visible
-            entry.target.classList.add('visible');
-            observer.unobserve(entry.target); // Stop observing once the class is added
-          }
-        });
-      }
-      // Create an intersection observer
-      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
-      // Select the target element
-      const targetElement = document.querySelector('.prefooter-wrapper');
-      // Start observing the target element
-      observer.observe(targetElement);
-    });
-  }
-}
-
 /* BACK TO TOP
 ------------------ */
 Drupal.behaviors.backToTop = {
