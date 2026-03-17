03723d9f1e933f8889acb3e714eb21bee30112d9
Matt Larson <larsomat@uwec.edu>
2026-03-17 14:14:30 -0500

    carousel refactor

--- web/themes/custom/citizen_dart/components/03-composites/paragraphs/content-carousel/content-carousel.js
+++ web/themes/custom/citizen_dart/components/03-composites/paragraphs/content-carousel/content-carousel.js
@@ -1,35 +1,32 @@
-
 (function($, Drupal, once) {
-
-	//Swipebox script for lightbox images.
 	Drupal.behaviors.contentCarousel = {
 		attach: function (context, settings) {
-			//Slider
 			$(once('contentCarousel', '.paragraph--type--content-carousel', context)).each(function(){
-        if(!$('.views-row',this).length){
-          $(this).closest('.block-field-content-carousel').remove();
-        }
-				$(document).ready(function(){
-					$('.paragraph--type--content-carousel .widget-wrapper', this).slick({
-						adaptiveHeight: false,
-						autoplay: false,
-            centerMode: false,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            responsive: [
-              {
-                breakpoint: 984,
-                settings: {
-                  adaptiveHeight: false,
-                  centerMode: true,
-                  centerPadding: '20px',
-                }
-              }
-            ]
-					});
+				// remove the whole paragraph if there are zero widgets to show
+				if(this.querySelector('.widget-wrapper').childElementCount <= 0){
+					this.closest('.block-field-content-carousel').remove();
+					return;
+				}
+
+				// do slick slider
+				$('.widget-wrapper', this).slick({
+					adaptiveHeight: false,
+					autoplay: false,
+					centerMode: false,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					responsive: [
+						{
+							breakpoint: 984,
+							settings: {
+								adaptiveHeight: false,
+								centerMode: true,
+								centerPadding: '20px',
+							}
+						}
+					]
 				});
 			});
 		}
 	}
-
 })(jQuery, Drupal, once);
